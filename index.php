58df62fef382f86c304f143cdad36fd670e49d5c
vikkivins <victoria.oliveira2016@hotmail.com>
2022-12-12 03:44:55 -0300

    a

--- index.php
+++ index.php
@@ -4,7 +4,42 @@ $nome = $_SESSION['usuario'];
 require_once 'cabecalho.php';
 ?>
 
-
+<div class="row">
+<div class="col-sm-6">
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-6">
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
 
 <?php
     include_once 'rodape.php';
