097c8d7846be075c1be8d84ecd2406ec8de5f469
vikkivins <victoria.oliveira2016@hotmail.com>
2022-12-12 04:00:40 -0300

    a

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ require_once 'cabecalho.php';
 
 <div class="row">
 <div class="col-sm-6">
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="card mb-3" style="max-width: 800;">
     <div class="row g-0">
         <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
+        <img src="img/apocalipse1.png" class="img-fluid rounded-start" alt="Apocalipse Nuclear">
         </div>
         <div class="col-md-8">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Apocalipse Nuclear</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
@@ -24,14 +24,88 @@ require_once 'cabecalho.php';
 </div>
 <div class="row">
 <div class="col-sm-6">
-    <div class="card mb-3" style="max-width: 540px;">
+    <div class="card mb-3" style="max-width: 800;">
     <div class="row g-0">
         <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
+        <img src="img/apocalipse2.png" class="img-fluid rounded-start" alt="Apocalipse Meteoro">
         </div>
         <div class="col-md-8">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Apocalipse Meteoro</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-6">
+    <div class="card mb-3" style="max-width: 800;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="img/apocalipse3.png" class="img-fluid rounded-start" alt="Apocalipse Incendiário">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Apocalipse Incendiário</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-6">
+    <div class="card mb-3" style="max-width: 800;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="img/apocalipse4.png" class="img-fluid rounded-start" alt="Apocalipse Tornado">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Apocalipse Tornado</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-6">
+    <div class="card mb-3" style="max-width: 800;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="img/apocalipse5.png" class="img-fluid rounded-start" alt="Apocalipse Tsunami">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Apocalipse Tsunami</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-6">
+    <div class="card mb-3" style="max-width: 800;">
+    <div class="row g-0">
+        <div class="col-md-4">
+        <img src="img/apocalipse6.png" class="img-fluid rounded-start" alt="Apocalipse Zumbi">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Apocalipse Zumbi</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
