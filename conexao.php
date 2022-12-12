2b4f4c43c8148b540a580a4db957c96925293bdf
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-12-12 20:17:31 -0300

    a

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conecta= new PDO("mysql:dbname=db_exemplo;host=localhost", "root","");
+    $conecta= new PDO("mysql:dbname=victoria_db;host=localhost", "root","zwr1yxjkbw");
     try{
         $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
