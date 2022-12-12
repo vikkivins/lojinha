564d63a59ce1849f3d4bffc8670821c756e9786e
vikkivins <victoria.oliveira2016@hotmail.com>
2022-12-12 02:15:08 -0300

    a

--- cookie.php
+++ cookie.php
@@ -9,31 +9,37 @@
     echo "Código: $produto<br>";
     echo "<img src='img/apoca$produto.png' height='250' width='250'><br>";
    switch ($produto) {
+
       case 1:
-     
       $apocalipse = 'Apocalipse Nuclear';
       $preco = 23.248;
          break;
+
       case 2:
          $apocalipse = 'Apocalipse Meteoro';
          $preco = 20.199;
          break;
+
       case 3:
          $apocalipse = 'Apocalipse Incendiario';
          $preco = 18.327;
          break;
+
       case 4:
          $apocalipse = 'Apocalipse Tornado';
          $preco = 15.112;
          break;
+
       case 5:
          $apocalipse = 'Apocalipse Tsunami';
          $preco = 27.463;
          break;
+
       case 6:
          $apocalipse = 'Apocalipse Zumbi';
          $preco = 110.678;
          break;
+
       default:
          $apocalipse = "nenhum apocalipse selecionado";
          $preco = 0;
@@ -47,7 +53,7 @@
 
   }
   else{
-   $tenis = "nenhum tênis selecionado<br>";
+   $apocalipse = "nenhum apocalipse selecionado<br>";
    $preco = 0;
   }
   setcookie("produto", $produto, time()+3600);
