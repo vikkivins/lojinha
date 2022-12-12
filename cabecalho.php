6d85f4fd8b6877192d42fe63093222b7127044b0
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-12-12 03:18:59 -0300

    aa

--- cabecalho.php
+++ cabecalho.php
@@ -5,6 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    </head>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo_png.png" width="30" height="30" alt="">
+            </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Início</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Produtos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Sobre Nós</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Usuário</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Login/Sign-up</a>
+            </li>
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+        </form>
+        </div>
+    </div>
+    </nav>
